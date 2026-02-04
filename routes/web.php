74353eb2de174310dc7bb3d92b2510b29194792f
redouane <benbrahimredouane664@gmail.com>
2026-02-04 14:47:48 +0100

    Update web routes for new features

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,12 +13,17 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','active'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','active')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('categories',CategoryController::class);
+Route::resource('links',LinkController::class);
+Route::resource('tags',TagController::class);
+
 
 require __DIR__.'/auth.php';
