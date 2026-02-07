512ac2003ae215f99c5a9d299b64077f10cf5f17
redouane <benbrahimredouane664@gmail.com>
2026-02-07 19:45:13 +0100

    chore: adjust category model add hasmany

--- app/Models/category.php
+++ app/Models/category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class category extends Model
 {
@@ -11,4 +12,7 @@ class category extends Model
         'name',
         
     ];
+    public function links(){
+       return $this->hasMany(link::class);
+    }
 }
