0442d53626d30c9cc69b468759dbfe93a9539ac3
redouane <benbrahimredouane664@gmail.com>
2026-02-10 10:06:43 +0100

    chore:add belongsToMany to link model

--- app/Models/link.php
+++ app/Models/link.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class link extends Model
+class Link extends Model
 {
     //
     protected $fillable = ['name','url','category_id'];
@@ -14,6 +14,6 @@ class link extends Model
         return $this->belongsTo(category::class);
     }
     public function tags(){
-        return $this->belongsToMany(tag::class);
+        return $this->belongsToMany(Tag::class,'link_tag','link_id','tag_id');
     }
 }
