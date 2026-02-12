f8338b67f8c57a5bdc3ccdc7120fd3a21582e932
redouane <benbrahimredouane664@gmail.com>
2026-02-12 17:18:08 +0100

    add user_id to link model

--- app/Models/link.php
+++ app/Models/link.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use Illuminate\Container\Attributes\Tag;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Link extends Model
 {
     //
-    protected $fillable = ['name','url','category_id'];
+    protected $fillable = ['name','url','category_id','user_id'];
 
     public function category(){
-        return $this->belongsTo(category::class);
+        return $this->belongsTo(Category::class);
     }
     public function tags(){
         return $this->belongsToMany(Tag::class,'link_tag','link_id','tag_id');
