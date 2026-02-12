dd95ecd9d8047b04b40cd806d72c0cee723c1b97
redouane <benbrahimredouane664@gmail.com>
2026-02-12 17:26:58 +0100

    chore:maintenance

--- database/migrations/2026_02_08_111444_create_link_tag_table.php
+++ database/migrations/2026_02_08_111444_create_link_tag_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('link_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignId('link_id')->constrained()->onDelete('cascade');
-             $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
